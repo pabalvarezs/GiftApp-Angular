868bda35d670a711107a3d43f97acc1a4df66ca9
Pablo <pabloalvarezseguel@gmail.com>
2021-04-13 17:26:06 -0400

    seccion 7 clase 79

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent  {
 
-  constructor() { }
+  // el ! es un excepcion not null, ya que txtBuscar pudiese ser nulo
+  // el viewChild permite buscar un elemento del cual se hizo referencia local en el html
+  // devuelve un element ref que es de tipo T, por lo cual debemos hacer referencia a que tipo es el elemnto html, en este caso htmlInputElement
+  @ViewChild('txtBuscar') txtBuscar! : ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
-  }
-
+  buscar( ){
+      //  console.log(this.txtBuscar);
+      //  document.querySelector('input')?.value = '';   
+      const  valor = this.txtBuscar.nativeElement.value;
+      console.log(valor);     
+      this.txtBuscar.nativeElement.value = '';
+  }  
 }
